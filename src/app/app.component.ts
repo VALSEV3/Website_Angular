1bb506e7b4d5a6ce540049dbf000e52fc63b3f47
VALSEV3 <valeri.sevyan@tumo.org>
2024-08-26 18:05:15 +0400

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { MainComponent } from './main/main.component';
+import { PortfolioComponent } from "./portfolio/portfolio.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, MainComponent, PortfolioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
